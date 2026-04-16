3bfbf4e304fd6c1350da4246e887116af5871172
Zayd <zaydabdullahmohd@gmail.com>
2026-04-16 13:21:42 -0700

    Added circuit info feature | Bug fixes

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+
+window.addEventListener("DOMContentLoaded", () => {
+
+  fetch("https://api.openf1.org/v1/meetings?year=2026&country_name=Singapore")
+.then(res => res.json())
+.then(meetings => {
+if(!meetings.length) return;
+
+  const data=meetings[0];
+document.querySelector("#trackImage").src = data.circuit_image;
+document.querySelector("#raceName").innerText=
+data.meeting_name + " - " + data.location + ", " + data.country_name;
+
+})
+.catch(err => console.log(err));
+
+
+})
+
+
+
 const searchBox=document.querySelector("#searchBox");
 const tbody=document.querySelector("#driversTable tbody");
 fetch("https://api.openf1.org/v1/drivers?session_key=latest")
@@ -14,7 +35,7 @@ drivers.forEach((driver, index) => {
 const row =document.createElement("tr");
 row.classList.add("driverRow");
 row.innerHTML =`
-<td>${index +1}</rd>
+<td>${index + 1}</td>
 <td>${driver.full_name}</td>
 
 <td><img src="${driver.headshot_url}" width="65"></td>
@@ -60,6 +81,38 @@ const toggle=document.querySelector("#darkToggle");
 
 toggle.addEventListener("click", () => {
 document.body.classList.toggle("dark");
+
+
+});
+
+
+const headers= document.querySelectorAll("th[data-col]");
+headers.forEach(header=> {
+header.addEventListener("click", () => {
+const colIndex = header.getAttribute("data-col");
+const rows=Array.from(document.querySelectorAll(".driverRow"));
+rows.sort((a, b)=> {
+const A=a.children[colIndex].innerText.toLowerCase();
+const B =b.children[colIndex].innerText.toLowerCase();
+
+if(!isNaN(A) && !isNaN(B)) {
+return Number(A) - Number(B);
+}
+return A.localeCompare(B);
+
+
+});
+
+
+
+const tbody=document.querySelector("#driversTable tbody");
+tbody.innerHTML="";
+rows.forEach(r=> tbody.appendChild(r));
+
+
+});
+
+
 
 
 });
